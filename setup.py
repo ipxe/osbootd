75b102be367372b696f3f111b4fd7db06cb5f22c
Michael Brown <mbrown@fensystems.co.uk>
2017-07-26 13:30:29 +0100

    [setup] Add minimal setup.py file
    Signed-off-by: Michael Brown <mbrown@fensystems.co.uk>

---
+++ setup.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+wsgidir = '/usr/libexec/osbootd'
+httpconfdir = '/etc/httpd/conf.d'
+
+setup(
+    name="osbootd",
+    version="0.1",
+    description="Operating System Boot Image Daemon",
+    author="Michael Brown",
+    author_email="mbrown@fensystems.co.uk",
+    packages=['osbootd'],
+    data_files=[
+        (wsgidir, ['osbootd-wsgi']),
+        (httpconfdir, ['osbootd.conf']),
+        ],
+    )
