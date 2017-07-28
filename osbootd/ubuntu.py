8cb58b0edfdd1b80ee63d204734a6ae5f0d0740b
Michael Brown <mbrown@fensystems.co.uk>
2017-07-28 10:21:10 +0100

    [ubuntu] Add minimal support for Ubuntu "netboot" distributions
    Signed-off-by: Michael Brown <mbrown@fensystems.co.uk>

--- osbootd/ubuntu.py
+++ osbootd/ubuntu.py
@@ -1,6 +1,9 @@
 """An Ubuntu distribution"""
 
 import logging
+import re
+from collections import defaultdict
+from cStringIO import StringIO
 
 from cachetools.func import lru_cache
 from werkzeug.wrappers import Response
@@ -9,29 +12,67 @@ from osbootd.distro import Distro
 logger = logging.getLogger(__name__)
 
 
-class UbuntuLiveDistro(Distro):
+class UbuntuDistro(Distro):
+    """An Ubuntu distribution"""
+
+    @classmethod
+    def autodetect(cls, tree):
+        """Autodetect a distribution tree"""
+        return tree.exists('README.diskdefines')
+
+    @property
+    @lru_cache()
+    def diskdefines(self):
+        """Read the disk definition file"""
+        defs = defaultdict(str)
+        for line in StringIO(self.tree.read('README.diskdefines')):
+            m = re.match(r'^\s*#define\s+(?P<key>\w+)\s+(?P<val>.+?)\s*$', line)
+            if m:
+                defs[m.group('key')] = m.group('val')
+        return defs
+
+    @property
+    def name(self):
+        """Get distribution name"""
+        return self.diskdefines['DISKNAME'].split(' ', 1)[0]
+
+    @property
+    def version(self):
+        """Get distribution version"""
+        return self.diskdefines['DISKNAME'].split(' ', 1)[-1]
+
+
+class UbuntuNetbootDistro(UbuntuDistro):
+    """An Ubuntu netboot distribution"""
+
+    @classmethod
+    def autodetect(cls, tree):
+        """Autodetect a distribution tree"""
+        return (super(UbuntuNetbootDistro, cls).autodetect(tree) and
+                tree.exists('install/netboot'))
+
+    def ep_boot_ipxe(self, _request, _urls):
+        """Generate iPXE boot script"""
+        script = ''.join(x + '\n' for x in (
+            "#!ipxe",
+            "kernel install/netboot/ubuntu-installer/%(arch)s/linux"
+            " initrd=initrd.gz",
+            "initrd install/netboot/ubuntu-installer/%(arch)s/initrd.gz",
+            "boot",
+            )) % {
+                'arch': self.diskdefines['ARCH'],
+            }
+        return Response(script, content_type='text/plain')
+
+
+class UbuntuLiveDistro(UbuntuDistro):
     """An Ubuntu live distribution"""
 
     @classmethod
     def autodetect(cls, tree):
         """Autodetect a distribution tree"""
-        return tree.exists('.disk/info') and tree.exists('casper')
-
-    @property
-    @lru_cache()
-    def info(self):
-        """Read the disk information file"""
-        return self.tree.read('.disk/info')
-    
-    @property
-    def name(self):
-        """Get distribution name"""
-        return self.info.split(' ', 1)[0]
-
-    @property
-    def version(self):
-        """Get distribution version"""
-        return self.info.split(' ', 1)[1]
+        return (super(UbuntuLiveDistro, cls).autodetect(tree) and
+                tree.exists('casper'))
 
     def ep_boot_ipxe(self, _request, _urls):
         """Generate iPXE boot script"""
